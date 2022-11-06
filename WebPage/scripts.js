1cd96105f29e45635e7b25cd2dff386aa1e0add9
Retspeih <hlnguyen504@gmail.com>
2022-11-06 03:08:41 -0500

    Babel installed into package.json

--- WebPage/scripts.js
+++ WebPage/scripts.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App.js"
+import Babel from "babel"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const element = <h1>Hello, world</h1>;

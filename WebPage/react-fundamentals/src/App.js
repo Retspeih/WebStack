b2e3dd2a4057c86eba3d67ba03009f51d7196be0
Retspeih <hlnguyen504@gmail.com>
2022-11-06 19:32:53 -0500

    Base default website server running

---
+++ WebPage/react-fundamentals/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './Navbar';
+
+function App() {
+    return (
+        <>
+            <Navbar title="Lots of Components" />
+            <div className='bg-dark'>Hello, World!</div>
+        </>
+        
+    );
+}
+
+export default App;
